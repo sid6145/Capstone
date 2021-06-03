4ef2627ecfa49ab6e477aee3fbb48afad04fec57
sid6145 <sidtdesh@gmail.com>
2021-06-03 14:51:52 +0530

    login, signup page and a navbar

---
+++ client/src/Components/Header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Navbar, Nav} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import './style.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function Header (){
+  return(
+    <Navbar className="navbar" bg="dark" variant="dark">
+    <Navbar.Brand><Link to="/"><img src="images/logo.jpeg" className="logo-img"/></Link> Mental Chroma</Navbar.Brand>
+    <Nav className="ms-auto">
+      <Nav.Link >Home</Nav.Link>
+      <Nav.Link >Schedule Appoinment</Nav.Link>
+   <Nav.Link ><Link to="/fordoctors">For Doctors</Link></Nav.Link>
+      <Nav.Link >Community</Nav.Link>
+      <Nav.Link >Signin</Nav.Link>
+    </Nav>
+  </Navbar>
+  )
+    
+}
+
+export default Header
